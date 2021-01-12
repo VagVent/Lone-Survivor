bd268ddeeb95f5bbc8693ed50c4bc7e3d5682375
VagVent <57504597+VagVent@users.noreply.github.com>
2021-01-13 01:40:11 +0200

    Add jump control
    Add jump control to Hero.
    Add methods to check if Hero is on air to compute the gravity acceleration when he starts to fall.

--- Lone Survivor/Hero.java
+++ Lone Survivor/Hero.java
@@ -8,8 +8,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Hero extends Actor
 {
-    private int speed = 10;         // speed of the Hero's move
-    private int direction = 1;      // diraction=1 to right, diraction=-1 to left
+    private int speed = 10;           // speed of the Hero's move
+    private int direction = 1;        // diraction=1 to right, diraction=-1 to left
+    private int acceleration = 4;     // acceleration of gravity
+    private int jumpStrength = 30;    
+    private int vSpeed = 0;           // the speed of hero when he is jumpping
     
     // Arrays with asset of Hero
     private static final int NUM_OF_IMAGES = 7;
@@ -41,6 +44,7 @@ public class Hero extends Actor
     public void act() 
     {
         checkKeys();
+        checkFall();
     }    
     
     /**
@@ -48,14 +52,15 @@ public class Hero extends Actor
      */
     private void checkKeys()
     {
-
+        // press right arrow to move right
         if (Greenfoot.isKeyDown("right")) 
         {
             moveRight();
             switchImage(rightImages);
             direction = 1;
         }
-
+        
+        // press left arrow to move left
         if (Greenfoot.isKeyDown("left"))
         {
             moveLeft();
@@ -63,24 +68,16 @@ public class Hero extends Actor
             direction = -1;
         }
         
-    }
-    
-    /**
-     * Move Hero at right way when press the right arrow.
-     */
-    private void moveRight() 
-    {
-        //Greenfoot.playSound("walking1_man_sort.wav");
-        move(speed);
-    }
+        // press up arrow to jump
+        if (Greenfoot.isKeyDown("up"))
+        {
+            // jump only if Hero is on ground
+            if (onGround())
+            {
+                jump();
+            }
 
-    /**
-     * Move Hero at left way when press the left arrow.
-     */
-    private void moveLeft()
-    {
-        //Greenfoot.playSound("walking1_man_sort.wav");
-        move(-speed);
+        }
     }
     
     /**
@@ -91,5 +88,64 @@ public class Hero extends Actor
         currentImage = (currentImage + 1) % NUM_OF_IMAGES;
         setImage(table[currentImage]);
         return table;
+    }
+    
+    /**
+     * Move Hero at right way when press the right arrow.
+     */
+    private void moveRight() 
+    {
+        move(speed);
+    }
+
+    /**
+     * Move Hero at left way when press the left arrow.
+     */
+    private void moveLeft()
+    {
+        move(-speed);
+    }
+    
+    /**
+     * Hero can jump 
+     */
+    private void jump()
+    {
+        vSpeed = -jumpStrength;
+        fall();
+    }
+    
+    /**
+     * Check if Hero is on ground.
+     */
+    private boolean onGround() 
+    {
+        Actor under = getOneObjectAtOffset(0, getImage().getHeight()/2, Ground.class);        
+        return under != null;
+    }
+    
+    /**
+     * Check if Hero falls to add speed of gravity acceleration.
+     */
+    private void checkFall() 
+    {
+        if (onGround())
+        {
+            vSpeed = 0;            
+        }  
+
+        else 
+        {
+            fall();
+        }
+    }
+    
+    /**
+     * Fall Hero after the jump with gravity acceleration.
+     */
+    private void fall()
+    {
+        setLocation(getX(), getY() + vSpeed);
+        vSpeed = vSpeed + acceleration;
     }
 }
