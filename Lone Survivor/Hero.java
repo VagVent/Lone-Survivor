66ff1cb45637f8724726daa2e2aea00f22748cf8
VagVent <57504597+VagVent@users.noreply.github.com>
2021-01-13 01:00:39 +0200

    Add move controls to Hero
    Add move controls to Hero. The Hero can move right and left way into the 1st level world.
    Corrected the picture asset of the Hero.

--- Lone Survivor/Hero.java
+++ Lone Survivor/Hero.java
@@ -1,19 +1,94 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Hero here.
+ * Hero υλοποιεί τον παίχτη στο παιχνίδι.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author VangVent
+ * @version (31/5/2020)
  */
 public class Hero extends Actor
 {
+    private int speed = 10;
+    private int direction = 1;      // diraction=1 to right, diraction=-1 to left
+    
+    // Arrays with asset of Hero
+    private static final int NUM_OF_IMAGES = 7;
+    private GreenfootImage[] rightImages;
+    private GreenfootImage[] leftImages;   
+    private int currentImage;
+    
+    public Hero()
+    {
+
+        rightImages = new GreenfootImage[NUM_OF_IMAGES];
+        leftImages = new GreenfootImage[NUM_OF_IMAGES];
+
+        for (int i = 0; i < NUM_OF_IMAGES; i++) 
+        {
+            rightImages[i] = new GreenfootImage ("walk_right_0" + i + ".png");
+            leftImages[i] = new GreenfootImage ("walk_left_0" + i + ".png");
+        }
+
+        currentImage = 0;
+        setImage(rightImages[currentImage]);        
+    }
+    
     /**
      * Act - do whatever the Hero wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        checkKeys();
     }    
+    
+    /**
+     * Check of keys press to move Hero.
+     */
+    private void checkKeys()
+    {
+
+        if (Greenfoot.isKeyDown("right")) 
+        {
+            moveRight();
+            switchImage(rightImages);
+            direction = 1;
+        }
+
+        if (Greenfoot.isKeyDown("left"))
+        {
+            moveLeft();
+            switchImage(leftImages);
+            direction = -1;
+        }
+        
+    }
+    
+    /**
+     * Move Hero at right way when press the right arrow.
+     */
+    private void moveRight() 
+    {
+        //Greenfoot.playSound("walking1_man_sort.wav");
+        move(speed);
+    }
+
+    /**
+     * MOve Hero at left way when press the left arrow.
+     */
+    private void moveLeft()
+    {
+        //Greenfoot.playSound("walking1_man_sort.wav");
+        move(-speed);
+    }
+    
+    /**
+     * Switch the pictures of Hero's asset.
+     */
+    private GreenfootImage[] switchImage(GreenfootImage[] table)
+    {
+        currentImage = (currentImage + 1) % NUM_OF_IMAGES;
+        setImage(table[currentImage]);
+        return table;
+    }
 }
