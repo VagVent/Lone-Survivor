8c615f354055ef760c122467db6566f63184f123
VagVent <57504597+VagVent@users.noreply.github.com>
2021-01-16 11:51:50 +0200

    Initialize 2nd level
    Setting the background of the level.
    Setting the ground of the level.
    Add the Hero and apply the scrolling feature.

---
+++ Lone Survivor/Level_2_World.java
@@ -0,0 +1,100 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * 2nd Level
+ * 
+ * @author Vangelis Ventoulis
+ * @version 
+ */
+public class Level_2_World extends World
+{
+    private static final int WIDTH = 1000; // width viewport
+    private static final int HEIGHT = 600; // height viewport
+
+    private Hero hero;    
+    private Scroller scroller;
+    
+    private Counter counter;    
+    private HealthBar healthbar;
+
+    /**
+     * Constructor for objects of class Level_2_World.
+     */
+    public Level_2_World()
+    {    
+        // create a new world with 1000*600 such as the viewport cells with a cell 
+        // size of 1x1 pixels.
+        super(WIDTH, HEIGHT, 1, false);
+
+        // Setup the background image of the 1st level.
+        GreenfootImage backGround = new GreenfootImage("bg_Level2_4000x600.png");
+
+        int backGroundWidth = backGround.getWidth();   // scrolling image width
+        int backGroundHeigth = backGround.getHeight(); // scrolling image height
+
+        // creates the Scroller object
+        scroller = new Scroller(this, backGround, backGroundWidth, backGroundHeigth);
+
+        // prepare the new level with the hero and with the point counter which we have
+        // from the previous level
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        // add the road as the ground of the 2nd level
+        Ground ground = new Ground();
+        addObject(ground,1501, 600);
+        ground.setImage("road.png");
+        ground.getImage().scale(5000, 100);
+
+        // add the Hero to world
+        hero = new Hero();
+        addObject(hero, 55, 500);
+
+        // add the counter into the wprld
+        counter = new Counter("Score: ");
+        // update the counter with the value from the previous level
+        //counter.setValue(points);
+        addObject(counter, 65, 25);
+
+        // add Hero's healthbar into the world
+        healthbar = new HealthBar();
+        addObject(healthbar, 65, 60);
+
+        /******************************************************************
+         * 
+         * Creation of the zombies at 1st level.
+         * 
+         *****************************************************************/
+    }
+
+    /**
+     * This method is called whenever the 'Act' or 'Run' button gets pressed
+     * in the environment.
+     */
+    public void act()
+    {
+        if (hero != null)
+        {
+            scroll();
+        }
+    }
+
+    /**
+     * This method is to scroll the viewport. The center of the viewport is the Hero.
+     */
+    private void scroll() 
+    {
+        int dsx = hero.getX() - WIDTH / 2;
+        int dsy = hero.getY() - HEIGHT / 2;
+
+        scroller.scroll(dsx, dsy);
+    }
+
+    
+}
