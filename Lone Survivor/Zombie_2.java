ec108b9e9531e850072fe1ec3ff9ef1f5fd95a17
VagVent <57504597+VagVent@users.noreply.github.com>
2021-01-17 13:34:12 +0200

    Add images-asset for dying Zombie 2
    Add images at asset.
    
    Add arrays to store the images at  Zombie_2 class.

--- Lone Survivor/Zombie_2.java
+++ Lone Survivor/Zombie_2.java
@@ -11,21 +11,26 @@ public class Zombie_2 extends Enemy
     private int direction = -1; // direction of zombie avatar to left. -1 is for left and 1 for right
     private int speed = 5;      // speed of the moving zombie
     private int walkSteps = 0;  // a counter to change direction
+    private int health = 150;    // the health/life of the zombie
+    private int points = 50;     // which gain the hero when he kills this zombie
 
     // store the images-asset of 1st zombie
     private static final int NUM_OF_IMAGES = 12;
     private GreenfootImage[] rightImages = new GreenfootImage[NUM_OF_IMAGES];
     private GreenfootImage[] leftImages = new GreenfootImage[NUM_OF_IMAGES];
+    private GreenfootImage[] rightImagesDying = new GreenfootImage[NUM_OF_IMAGES];
+    private GreenfootImage[] leftImagesDying = new GreenfootImage[NUM_OF_IMAGES];
 
     private int currentImage; // variable to restart from the 1st image
 
     public Zombie_2() 
     {
-
         for (int i = 0; i < NUM_OF_IMAGES; i++) 
         {
             rightImages[i] = new GreenfootImage ("Zombie_02_Walking_right_0" + i + ".png");
             leftImages[i] = new GreenfootImage ("Zombie_02_Walking_left_0" + i + ".png");
+            rightImagesDying[i] = new GreenfootImage ("Zombie_02_Dying_right_0" + i + ".png");
+            leftImagesDying[i] = new GreenfootImage ("Zombie_02_Dying_left_0" + i + ".png");  
         }
 
         currentImage = 0;
@@ -59,7 +64,7 @@ public class Zombie_2 extends Enemy
         walkSteps++;
     }
 
-     /**
+    /**
      * Method for walking left.
      */
     private void walkLeft() 
@@ -77,5 +82,59 @@ public class Zombie_2 extends Enemy
         move(speed);
         switchImage(rightImages, NUM_OF_IMAGES);
         direction = 1;
+    }
+
+    /**
+     * Method to consume the damage of the bullet's hit.
+     */
+    public void hurt(int damage) 
+    {
+        health = health - damage;
+        
+        // sound of the zombie when it is hit by a bullet
+        Greenfoot.playSound("blood_hitting_zombie.wav");
+        
+        if (direction == -1)
+        {
+            setImage("Zombie_02_Dying_left_01.png");
+        }
+        
+        else if (direction == 1) 
+        {
+            setImage("Zombie_02_Dying_right_01.png");
+        }
+        
+        if (health <= 0)
+        {
+            die();
+        }
+    }
+
+    /**
+     * Method for dead zombie.
+     */
+    private void die() 
+    {
+        // sound of the zombie when it is dying
+        Greenfoot.playSound("zombie_01_dies.wav");
+        
+        if (direction == -1) 
+        {
+            switchImage(leftImagesDying, NUM_OF_IMAGES);
+            setLocation(getX(), getY() + 3);
+        }
+
+        else if (direction == 1)
+        {
+            switchImage(rightImagesDying, NUM_OF_IMAGES);
+            setLocation(getX(), getY() + 3);
+        }
+        
+        // to adding the dead zombies' points to the counter
+        Level_1_World world = (Level_1_World)getWorld();
+        world.addPointToCounter(points);
+        
+        // remove the dead zombie of the world
+        getWorld().removeObject(this);
     }
 }
